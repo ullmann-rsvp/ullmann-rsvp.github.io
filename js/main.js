a9eb4875cc3bf2d4e2653e29e9a6ceaaf9f71979
Zacky Ma <marchbox@gmail.com>
2015-03-10 17:35:34 -0700

    Better form usability.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-(function() {})();
+(function() {
+
+  var guestsEl = document.getElementById('guests');
+  var guestButtonCouple = document.getElementById('guest-button-couple');
+  var guestButtonFamily = document.getElementById('guest-button-family');
+
+  guestButtonCouple.onclick = function() {
+    guestsEl.setAttribute('data-size', 'couple');
+  };
+
+  guestButtonFamily.onclick = function() {
+    guestsEl.setAttribute('data-size', 'family');
+  };
+
+})();
