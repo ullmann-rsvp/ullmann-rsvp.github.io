d2e9f446e37df5034204ccd26c08c042039de8d1
Zacky Ma <marchbox@gmail.com>
2015-03-12 23:34:45 -0700

    Always show name of the first guest; form validation.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,34 @@
 (function() {
 
+  var mainEl = document.getElementsByTagName('main')[0];
+  var acceptedEl = document.getElementById('f-lead-yes');
+  var rejectedEl = document.getElementById('f-lead-no');
+  var formEl = document.getElementsByTagName('form')[0];
   var guestsEl = document.getElementById('guests');
   var guestButtonCouple = document.getElementById('guest-button-couple');
   var guestButtonFamily = document.getElementById('guest-button-family');
+  var guest1NameEl = document.getElementById('f-g1-name');
+  var submitEl = document.getElementById('submit');
+
+  formEl.onsubmit = function() {
+    if ((!acceptedEl.checked && !rejectedEl.checked) || !guest1NameEl.value) {
+      alert('Please either accept or reject this invitation, and tell us your' +
+        ' name');
+      return false;
+    }
+  };
+
+  acceptedEl.onchange = function() {
+    if (this.checked) {
+      mainEl.setAttribute('data-accepted', 'true');
+    }
+  };
+
+  rejectedEl.onchange = function() {
+    if (this.checked) {
+      mainEl.setAttribute('data-accepted', 'false');
+    }
+  };
 
   guestButtonCouple.onclick = function() {
     guestsEl.setAttribute('data-size', 'couple');
